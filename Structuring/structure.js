7c9a6583251c456c065b955c4135a6bb8dc56a0b
Aswene Sivaraj <ashsiva4@hotmail.co.uk>
2021-10-13 12:49:59 +0100

    uploaded completed calculator

---
+++ Structuring/structure.js
@@ -0,0 +1,28 @@
+'use strict';
+
+(function(){
+    /*
+    *
+    * Variables 
+    *
+    */
+
+    let element = document.querySelector('#app');
+
+    /*
+    *
+    * Methods 
+    *
+    */
+    let handleClicks = function(event){
+        console.log(event.target);
+    }
+
+    /*
+    *
+    * Inits & Event Listeners 
+    *
+    */
+
+    element.addEventListener('click',handleClicks);
+})();
